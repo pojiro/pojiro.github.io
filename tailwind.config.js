0a3667d90defe93dccd222ce5c27f2e9cc98060f
Ryota Kinukawa <pojiro.jp@gmail.com>
2022-10-09 17:41:19 +0900

    add: Inter font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./_layouts/**/*.html", "./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography')
