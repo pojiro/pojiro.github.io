badf41732763dbd7bda3b6ce91e76fcefc87acbc
Ryota Kinukawa <pojiro.jp@gmail.com>
2022-11-13 16:52:15 +0900

    udpate: .fwatch.exs for v0.2.2

--- .fwatch.exs
+++ .fwatch.exs
@@ -5,6 +5,8 @@ config :file_watch,
   dirs: ["."],
   # commands to be executed when detected, multiple commands can be specified
   commands: ["make tailwind"],
+  # if true, commands are executed in parallel
+  parallel_exec: false,
   # path detecting patterns, should be written in regex
   # refs. https://hexdocs.pm/elixir/Regex.html
   patterns: [
@@ -18,4 +20,9 @@ config :file_watch,
 
 config :logger,
   # use :debug to show matched path and ran command
-  level: :none
+  #     :none  to suppress logs
+  level: :debug
+
+# On Windows :file_system needs inotifywait.exe,
+# uncomment the following line and configure path to it
+# config :file_system, :fs_windows, executable_file: "path/to/inotifywait.exe"
