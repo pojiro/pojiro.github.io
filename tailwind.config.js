47de24d0a0c6798e711c79285ba60790942b8071
Ryota Kinukawa <pojiro.jp@gmail.com>
2022-11-18 10:01:24 +0900

    update: default.html, add style to sidebar's scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,5 +28,10 @@ module.exports = {
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/line-clamp"),
+    function ({ addVariant }) {
+      addVariant("scrollbar", "&::-webkit-scrollbar");
+      addVariant("scrollbar-track", "&::-webkit-scrollbar-track");
+      addVariant("scrollbar-thumb", "&::-webkit-scrollbar-thumb");
+    },
   ],
 };
